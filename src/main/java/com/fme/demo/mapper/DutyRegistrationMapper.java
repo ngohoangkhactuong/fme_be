c97f3187d8e1d4139d1a27107d138ac61dc0f794
khactuong.ngohoang <khactuong.ngohoang@citynow.vn>
2025-12-26 17:40:01 +0700

    feat: Enhance DutyRegistrationMapper with list conversion methods

--- src/main/java/com/fme/demo/mapper/DutyRegistrationMapper.java
+++ src/main/java/com/fme/demo/mapper/DutyRegistrationMapper.java
@@ -5,13 +5,20 @@ import com.fme.demo.dto.dutyRegistration.DutyRegistrationResponse;
 import com.fme.demo.entity.DutyRegistration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface DutyRegistrationMapper {
 
   @Mapping(target = "id", ignore = true)
   @Mapping(target = "registrationTimestamp", ignore = true)
   DutyRegistration toEntity(DutyRegistrationRequest request);
 
+  List<DutyRegistration> toEntityList(List<DutyRegistrationRequest> requests);
+
   DutyRegistrationResponse toDto(DutyRegistration dutyRegistration);
+
+  List<DutyRegistrationResponse> toDtoList(List<DutyRegistration> dutyRegistrations);
 }
